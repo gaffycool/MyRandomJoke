d279aa3773a1ef7c32bded8579b3ef3fab6b774b
gaffycool <gaffy_cool@hotmail.com>
2018-03-15 13:26:48 +0000

    .
    .

--- app/src/main/java/com/example/tae/myrandomjoke/random_jokes/IRandomJokesPresenter.java
+++ app/src/main/java/com/example/tae/myrandomjoke/random_jokes/IRandomJokesPresenter.java
@@ -6,7 +6,8 @@ import com.example.tae.myrandomjoke.ui.base.MvpPresenter;
  * Created by TAE on 22-Feb-18.
  */
 
-public interface IRandomJokesPresenter   <V extends IRandomJokeMvpView> extends MvpPresenter<V> {
+public interface IRandomJokesPresenter
+        <V extends IRandomJokeMvpView> extends MvpPresenter<V> {
 
     void loadRandomJokes();
 
